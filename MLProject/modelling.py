c33b62427cfc74868dcb3c5ef6d359913765fcba
Rafly <mochamadrafly1102@gmail.com>
2026-06-06 10:19:11 +0700

    fix: ubah nama file MLProject menjadi MLproject sesuai standar

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,7 +3,6 @@ import mlflow
 import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import os
 
 # 1. Load dataset
 train = pd.read_csv("ai4i2020_preprocessing/train_clean.csv")
@@ -17,27 +16,13 @@ y_test = test["Machine failure"]
 
 print("Data berhasil dimuat!")
 
-# 2. Set experiment (Kembalikan ke 'file:./mlruns' untuk kebutuhan otomasi CI)
+# 2. Set experiment
 mlflow.set_tracking_uri("file:./mlruns")
 mlflow.set_experiment("Predictive_Maintenance_Basic")
 
-# 3. Aktifkan autolog
-# mlflow.autolog()
-
 # 4. Jalankan training
 print("\nMemulai training model...")
-
-# TRIK KHUSUS GITHUB CI: Jika berjalan di GitHub Actions, kita buat run bohong-bohongan agar tidak konflik
-if os.environ.get("GITHUB_ACTIONS") == "true":
-    class DummyRun:
-        def __enter__(self): return self
-        def __exit__(self, exc_type, exc_val, exc_tb): pass
-    run_context = DummyRun()
-else:
-    # Jika di laptop lokal kamu, tetap pakai mlflow.start_run() asli
-    run_context = mlflow.start_run()
-
-with run_context:
+with mlflow.start_run():
 
     model = RandomForestClassifier(
         random_state=42,
@@ -51,8 +36,7 @@ with run_context:
 
     print("Accuracy Model:", accuracy)
     
-    # Mencatat metrik testing ke tracking local server CI (Hanya aktif jika tidak di GitHub CI)
-    if os.environ.get("GITHUB_ACTIONS") != "true":
-        mlflow.log_metric("testing_accuracy", accuracy)
+    # Mencatat metrik testing ke tracking local server
+    mlflow.log_metric("testing_accuracy", accuracy)
 
 print("\n[SUKSES] Model selesai dilatih dan dicatat oleh MLflow Project!")
