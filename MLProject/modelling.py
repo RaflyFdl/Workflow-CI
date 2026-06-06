6e6a80cb9540ebbcccbd8b07b6efb18cc178afa0
Rafly <mochamadrafly1102@gmail.com>
2026-06-06 10:23:36 +0700

    fix: pisahkan logika start_run untuk github ci

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,6 +3,7 @@ import mlflow
 import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import os
 
 # 1. Load dataset
 train = pd.read_csv("ai4i2020_preprocessing/train_clean.csv")
@@ -22,21 +23,26 @@ mlflow.set_experiment("Predictive_Maintenance_Basic")
 
 # 4. Jalankan training
 print("\nMemulai training model...")
-with mlflow.start_run():
 
-    model = RandomForestClassifier(
-        random_state=42,
-        class_weight="balanced"
-    )
+# Cek apakah ini di GitHub Actions
+is_github_ci = os.environ.get("GITHUB_ACTIONS") == "true"
 
+if is_github_ci:
+    # Di GitHub CI, jangan pakai start_run() karena mlflow run . sudah membuatkannya secara otomatis
+    model = RandomForestClassifier(random_state=42, class_weight="balanced")
     model.fit(X_train, y_train)
-
     y_pred = model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
-
     print("Accuracy Model:", accuracy)
-    
-    # Mencatat metrik testing ke tracking local server
     mlflow.log_metric("testing_accuracy", accuracy)
+else:
+    # Di laptop lokal kamu, tetap berjalan normal pakai start_run()
+    with mlflow.start_run():
+        model = RandomForestClassifier(random_state=42, class_weight="balanced")
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        print("Accuracy Model:", accuracy)
+        mlflow.log_metric("testing_accuracy", accuracy)
 
 print("\n[SUKSES] Model selesai dilatih dan dicatat oleh MLflow Project!")
